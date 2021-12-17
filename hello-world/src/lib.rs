81cc4e7049f9c69b1f5d0d00ba4c9b022262227f
Lukaz Precision <30133485+lukazvprecisn@users.noreply.github.com>
2021-12-17 20:27:31 +0700

    hello world

---
+++ hello-world/src/lib.rs
@@ -0,0 +1,18 @@
+use solana_program::{
+  account_info::{
+    AccountInfo
+  },
+  entrypoint::ProgramResult,
+  msg,
+  pubkey::Pubkey,
+};
+
+pub fn process_instruction(
+  _program_id: &Pubkey,
+  _accounts: &[AccountInfo],
+  _instruction_data: &[u8],
+) -> ProgramResult {
+  msg!("process_instruction");
+
+  Ok(())
+}
